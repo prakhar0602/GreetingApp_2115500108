afd97632e5ab5c1d4cfb84a0b8b6ba7ce4883abf
prakhar0602 <prakhar06092002@gmail.com>
2025-03-06 16:13:16 +0530

    [Prakhar Gupta] Implemented: Simple Greeting in Buisness Layer in UC2

--- GreetingApp/Program.cs
+++ GreetingApp/Program.cs
@@ -1,3 +1,5 @@
+using BuisnessLayer.Interface;
+using BuisnessLayer.Service;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IGreetingBL, GreetingBL>();
 
 var app = builder.Build();
 
